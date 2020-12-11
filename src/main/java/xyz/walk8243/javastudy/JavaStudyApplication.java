94e8da6fc2eec16472fa6242979134f07ea96481
walk8243 <walk8243@gmail.com>
2020-12-12 02:50:51 +0900

    実行時間を計測

--- src/main/java/xyz/walk8243/javastudy/JavaStudyApplication.java
+++ src/main/java/xyz/walk8243/javastudy/JavaStudyApplication.java
@@ -1,12 +1,13 @@
 package xyz.walk8243.javastudy;
 
-import java.util.ArrayList;
-import java.util.stream.Collectors;
+// import java.util.ArrayList;
+// import java.util.stream.Collectors;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.StopWatch;
 
 import xyz.walk8243.javastudy.lib.PrimeNumber;
 
@@ -17,9 +18,13 @@ public class JavaStudyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JavaStudyApplication.class, args);
 
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
 		PrimeNumber primeNumber = new PrimeNumber();
-		ArrayList<Integer> array = primeNumber.get(100);
-		logger.info(array.stream().map((i) -> i.toString()).collect(Collectors.joining(",")));
+		primeNumber.get((int)Math.pow(2, 16));
+		stopWatch.stop();
+		logger.info("実行時間 => {}ms", stopWatch.getLastTaskTimeMillis());
+		// logger.info(array.stream().map((i) -> i.toString()).collect(Collectors.joining(",")));
 	}
 
 }
