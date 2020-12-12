81b00d7a1dee17859f8c6402cb5319fc0b8d382e
walk8243 <walk8243@gmail.com>
2020-12-12 21:41:10 +0900

    WebAPIのチュートリアルを実施

--- src/main/java/xyz/walk8243/javastudy/JavaStudyApplication.java
+++ src/main/java/xyz/walk8243/javastudy/JavaStudyApplication.java
@@ -4,10 +4,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.util.StopWatch;
-
-import xyz.walk8243.javastudy.controller.NormalController;
-import xyz.walk8243.javastudy.controller.ThreadController;
 
 @SpringBootApplication
 public class JavaStudyApplication {
@@ -15,26 +11,6 @@ public class JavaStudyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaStudyApplication.class, args);
-
-		StopWatch stopWatch = new StopWatch();
-
-		stopWatch.start();
-		try {
-			ThreadController controller = new ThreadController();
-			controller.execute();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		stopWatch.stop();
-		logger.info("並列処理 実行時間 => {}ms", stopWatch.getLastTaskTimeMillis());
-
-		stopWatch.start();
-		{
-			NormalController controller = new NormalController();
-			controller.execute();
-		}
-		stopWatch.stop();
-		logger.info("直列処理 実行時間 => {}ms", stopWatch.getLastTaskTimeMillis());
 	}
 
 }
