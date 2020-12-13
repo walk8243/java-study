5fb58e804486ef02dab8d33d3131f3dcff0c95c7
walk8243 <walk8243@gmail.com>
2020-12-13 15:41:31 +0900

    定期実行をしてみる

--- src/main/java/xyz/walk8243/javastudy/JavaStudyApplication.java
+++ src/main/java/xyz/walk8243/javastudy/JavaStudyApplication.java
@@ -1,13 +1,12 @@
 package xyz.walk8243.javastudy;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class JavaStudyApplication {
-	static Logger logger = LogManager.getLogger();
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaStudyApplication.class, args);
---
+++ src/main/java/xyz/walk8243/javastudy/ScheduledTasks.java
@@ -0,0 +1,27 @@
+package xyz.walk8243.javastudy;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTasks {
+	private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+
+	private static final SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+
+	@Scheduled(fixedRate = 5000)
+	public void reportCurrentTime() {
+		log.info("The time is now {}", timeFormat.format(new Date()));
+	}
+
+	@Scheduled(cron = "*/10 * * * * *")
+	public void reportCurrentDate() {
+		log.info("The date is now {}", dateFormat.format(new Date()));
+	}
+}
